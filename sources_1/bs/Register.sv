f929d152e687d93b9762d50d6da665ddeddf1e0a
Darian Primer <darianprimer@gmail.com>
2018-11-27 11:51:05 -0800

    FSM and Master top file. Start of slave top in progress

---
+++ sources_1/bs/Register.sv
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: Cal Poly SLO CPE 133
+// Engineer: Darian Primer
+// 
+// Create Date: 10/25/2018 09:19:27 AM
+
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Register #(parameter WIDTH = 4)
+    (input clk,
+    input en,
+    input clr,
+    input [WIDTH-1:0] D,
+    output logic [WIDTH-1:0] Q
+    );
+    
+    always_ff @ (posedge clk, negedge clr)
+    begin
+        if(clr)
+            Q=0;
+        else if(en)
+            Q=D;
+    end
+    
+endmodule
---
+++ sources_1/bs/Slave_Top.sv
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: Cal Poly SLO CPE 133
+// Engineer: Darian Primer
+// 
+// Create Date: 11/20/2018 01:22:30 AM
+// Design Name: Top file for Master basys board(player A).
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Slave_Top(
+    input [15:0] A,
+    input [15:0] B,
+    input LDR1B,
+    input LDR2B,
+    input clr,
+    input DispB,
+    output LivB,
+    output OKB,
+    output [15:0] B_Attack,
+    output [7:0] seg,
+    output [3:0] an,
+    output BTN1B,
+    output BTN2B,
+    output BTN3B
+    );
+    
+    
+
+
+
+endmodule
