f926822daddb9269f5846fe1f9b9e0a0fb0bddeb
Darian Primer <dprimer@calpoly.edu>
2018-12-03 10:22:00 -0800

    10Bit version of Master_Top, Slave_Top, and constraint files for both

--- sources_1/bs/Slave_Top10Bit.sv
+++ sources_1/bs/Slave_Top10Bit.sv
@@ -4,14 +4,14 @@
 // Engineer: Darian Primer
 // 
 // Create Date: 11/20/2018 01:22:30 AM
-// Design Name: 8 Bit Top file for Master basys board(player A).
+// Design Name: 10 Bit Top file for Master basys board(player A).
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module Slave_Top8Bit(
+module Slave_Top10Bit(
     input clk,
-    input [7:0] A,
-    input [7:0] B,
+    input [9:0] A,
+    input [9:0] B,
     //input LDR1B,
     //LDR1B is alwasy high because of delays to loading register one.
     //Register still holds correct value due to logic of the ckt.
@@ -24,7 +24,7 @@ module Slave_Top8Bit(
     input BTN3B_IN,
     output LivB,
     output OKB,
-    output [7:0] B_Attack,
+    output [9:0] B_Attack,
     output [7:0] seg,
     output [3:0] an,
     output BTN1B,
@@ -32,20 +32,20 @@ module Slave_Top8Bit(
     output BTN3B
     );
     
-    logic [7:0] t1, t2, t3, t4, t5, t6;
+    logic [9:0] t1, t2, t3, t4, t5, t6;
     logic t7, t8;
     
     //Decides if Register 0ne B is loading Ship positions or alive positions
-    mux2_1 #8 mux1(.A(B), .B(t6), .SEL(ST), .an(t1));
+    mux2_1 #10 mux1(.A(B), .B(t6), .SEL(ST), .an(t1));
     
     //This register saves the ship positions of player B
-    Register #8 R1B(.clk(clk), .D(t1), .en(1), .clr(clr), .Q(t2));
+    Register #10 R1B(.clk(clk), .D(t1), .en(1), .clr(clr), .Q(t2));
     
     //This register saves the attack positions of player B
-    Register #8 R2B(.clk(clk), .D(B), .en(LDR2B), .clr(clr), .Q(t4));
+    Register #10 R2B(.clk(clk), .D(B), .en(LDR2B), .clr(clr), .Q(t4));
     
     //This register saves the previous attack positions of player A to feed into the input checker.
-    Register #8 R3B(.clk(clk), .D(A), .en(t7), .clr(clr), .Q(t3));
+    Register #10 R3B(.clk(clk), .D(A), .en(t7), .clr(clr), .Q(t3));
     
     //The input checkers assures that player A only chose one new position to attack
     //InputChecker ICA(.A(t3), .B(A), .OK(t7));
@@ -56,7 +56,7 @@ module Slave_Top8Bit(
     assign t6 = t2 ^ t5;
     
     //Decides whether or not Player A is still alive
-    Comparator #8 comp1(.A(t2), .B(16'b0000000000000000), .EQ(t8), .LT(), .GT());
+    Comparator #10 comp1(.A(t2), .B(10'b0000000000), .EQ(t8), .LT(), .GT());
     
     //Ouptuts game status to sseg for player A
     Words2 words2A(.clk(clk), .wordSelect(DispB), .seg(seg), .an(an));
